317b56c3bf5b6df5e7e58575d62cac08fd26aa6e
Priyanshu-x <priyanshu18181@gmail.com>
2026-02-25 14:27:37 +0530

    fix: 502 Bad Gateway while deployement

--- start.sh
+++ start.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # Start Gunicorn in the background
-gunicorn -w 1 -k gevent --worker-connections 1000 --bind 127.0.0.1:5000 app:app &
+# We use GeventWebSocketWorker for proper SocketIO support with gevent
+gunicorn -w 1 -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker --worker-connections 1000 --bind 0.0.0.0:5000 app:app &
+
+# Wait a few seconds for Gunicorn to bind to the port
+sleep 3
 
 # Start Nginx in the foreground
 nginx -g "daemon off;"
